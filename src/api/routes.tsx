2b244bd5a4c2b09a41426a57294b8e3f3a74fecb
ibnuputraard <ibnu.putra@nawadata.id>
2024-02-26 00:28:11 +0700

    update favicon

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://127.0.0.1:8080/';
+export const BASE_URL =
+  'https://server.omdc.online/'; /*'http://127.0.0.1:8080/' */
 
 // AUTH
 export const LOGIN = 'user/login';
