125ca80fff4735cd1a5da92e985dfa572934f930
ibnuputraard <ibnu.putra@nawadata.id>
2024-07-11 14:27:49 +0700

    update for dev server

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -121,7 +121,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             alt="Logo"
           />
           <span className=" text-title-sm font-bold text-white">
-            OMDC v0.9.5 rev 2
+            OMDC v0.9.5 rev 3
           </span>
         </NavLink>
 
