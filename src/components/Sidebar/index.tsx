7ed493da41aa4011958de63f1592578ac64855d0
ibnuputraard <ibnu.putra@nawadata.id>
2024-12-18 17:04:05 +0700

    update list item and bank detail report

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -121,7 +121,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
             alt="Logo"
           />
           <span className=" text-title-sm font-bold text-white">
-            OMDC v0.9.5 rev 7
+            OMDC v0.9.5 rev 8.1
           </span>
         </NavLink>
 
